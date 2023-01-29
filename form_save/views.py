0bf823ecf080d8fee9557439aa10d1e7f3c5a638
amonfanticelli <amon_fanticelli@hotmail.com>
2023-01-29 17:51:00 -0300

    values divided by 100

--- form_save/views.py
+++ form_save/views.py
@@ -3,8 +3,6 @@ from django.shortcuts import render
 from .forms import UploadFileForm
 from rest_framework.views import APIView, Response
 from .models import Form
-from .serializers import FormSerializer
-import ipdb
 from django.db.models import Sum
 
 
@@ -32,7 +30,7 @@ def handle_uploaded_file(f):
             content = {
                 "type": content[:1],
                 "date": f"{content[1:5]}-{content[5:7]}-{content[7:9]}",
-                "value": content[10:19],
+                "value": int(content[10:19]) / 100,
                 "cpf": content[20:31],
                 "creditCard": content[31:42],
                 "time": f"{content[42:44]}:{content[44:46]}:{content[46:48]}",
@@ -48,6 +46,3 @@ class getAll(APIView):
             totalValue=Sum("value")
         )
         return Response(transactions)
-        # transactions = Form.objects.all()
-        # serializer = FormSerializer(transactions, many=True)
-        # return Response(serializer.data, status=201)
